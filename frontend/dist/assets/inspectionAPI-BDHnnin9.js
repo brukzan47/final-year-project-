feffc5903b583cfa5fbbf067ab481aab96ff0c9f
biruk bekele <brukb1958@gmail.com>
2026-07-02 18:53:22 +0300

    Rebuild frontend deploy assets

---
+++ frontend/dist/assets/inspectionAPI-BDHnnin9.js
@@ -0,0 +1,1 @@
+import{e as s}from"./index-6c17jwcA.js";const i={list:()=>s.get("/inspections"),create:t=>s.post("/inspections",t)};export{i as I};
--- frontend/dist/assets/shipmentAPI-CARFYOr-.js
+++
@@ -1,1 +0,0 @@
-import{e}from"./index-CS03OHzJ.js";const n={list:()=>e.get("/shipments"),create:t=>e.post("/shipments",t),update:(t,s)=>e.patch(`/shipments/${encodeURIComponent(t)}`,s)};export{n as S};
